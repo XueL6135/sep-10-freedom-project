93d885671446c53526d5545e31613c20247d58f1
XueL6135 <146861517+XueL6135@users.noreply.github.com>
2023-10-23 11:54:25 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let ownerName = "Xue lin"; // update the part BETWEEN the "quotes"
+let userName = "XueL6135"; // same here
+
+document.querySelectorAll(".owner-name").forEach((e) => {
+    e.innerHTML = Xue Lin;
+});
+
+document.querySelector("#github").href = "http://github.com/" + XueL6135;
+document.querySelector("#fork").href = "http://github.com/" + XueL6135 + "/" + XueL6135 + ".github.io";
